157cbb0fd7e73a301e25060872288208875c089c
Oumayma Saidi <your_email@example.com>
2025-03-15 16:13:38 +0000

    classe User

---
+++ src/main/java/Models/User.java
@@ -0,0 +1,31 @@
+package Models;
+
+public class User {
+    private int id;
+    private String nom;
+    private String email;
+    private String motDePasse;
+
+    public User() {}
+
+    public User(int id, String nom, String email, String motDePasse) {
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getMotDePasse() { return motDePasse; }
+    public void setMotDePasse(String motDePasse) { this.motDePasse = motDePasse; }
+}
+
